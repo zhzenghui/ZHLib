9dd0322d4bb3f3100604ed5b644b1cb716d9676c
zhzenghui <zhzenghui@gmail.com>
2014-09-19 14:11:17 +0800

    init zhlib

---
+++ ZHLib/ZHLib.h
@@ -0,0 +1,9 @@
+//
+//  ZHLib.h
+//  ZHLib
+//
+//  Created by bejoy on 14/9/19.
+//  Copyright (c) 2014年 zeng hui. All rights reserved.
+//
+
+#import "Message.h"
---
+++ ZHLib/ZHLib.m
@@ -0,0 +1,13 @@
+//
+//  ZHLib.m
+//  ZHLib
+//
+//  Created by bejoy on 14/9/19.
+//  Copyright (c) 2014年 zeng hui. All rights reserved.
+//
+
+#import "ZHLib.h"
+
+@implementation ZHLib
+
+@end
